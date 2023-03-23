3280cbab5727612cf96d1394ece3cc341e418a09
minjiwebui <minamall376@gmail.com>
2023-03-23 08:18:42 +0000

    z

--- js/droptop_main.js
+++ js/droptop_main.js
@@ -10,21 +10,28 @@ var swiper1 = new Swiper(".basic01", {
     slidesPerView: 1,
 });
 // sec02 슬라이드
-var swiper2 = new Swiper(".basic02", {
-    slidesPerView: 1.3,
-});
-swiper2.init();
+// var swiper2 = new Swiper(".basic02", {
+//     slidesPerView: 1.3,
+// });
 // sec 02 리사이징 시, 슬라이드 해제
-window.addEventListener('resize', () => {
-    let wiw = window.innerWidth;
-    if (wiw <= 1024) {
-        // 모바일에서만 스와이퍼 구현
-        swiper2.destroy(true, true);
-        
-    } else {
-        swiper2.init();
-        console.log('재실행');
+var swiper2 = undefined;
+    function initBenefit() {
+        if (wiw < 641 && swiper2 == undefined) {
+            swiper2 = new Swiper('.basic02', {
+                slidesPerView: 1.3,
+            });
+        } else if (wiw >= 640 && swiper2 != undefined) {
+            swiper2.destroy();
+            swiper2 = undefined;
+        }
     }
+    initBenefit();
+
+    var wiw = window.innerWidth;
+// 리사이징에 따라 반응형 잡기
+window.addEventListener('resize', () => {
+    wiw = window.innerWidth; //리사이징시 변경
+    initBenefit();
 });
 // header 모바일 서브메뉴(공통요소)
 $("nav > .fa-bars").click(function () {
