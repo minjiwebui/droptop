84cd89fd35b2af4ad0ddaf1786dff7b7a55d097a
minjiwebui <minamall376@gmail.com>
2023-03-24 02:26:24 +0000

    a

--- js/droptop_main.js
+++ js/droptop_main.js
@@ -16,19 +16,18 @@ var swiper2 = undefined;
             swiper2 = new Swiper('.basic02', {
                 slidesPerView: 1.3,
             });
-        } else if (wiw >= 640 && swiper2 != undefined) {
+        } else if (wiw <= 1024 && swiper2 != undefined) {
             swiper2.destroy();
             swiper2 = undefined;
         }
     }
-    initBenefit();
-
     var wiw = window.innerWidth;
 // 리사이징에 따라 반응형 잡기
 window.addEventListener('resize', () => {
     wiw = window.innerWidth; //리사이징시 변경
     initBenefit();
 });
+initBenefit();
 // header 모바일 서브메뉴(공통요소)
 $("nav > .fa-bars").click(function () {
     $(".navmb").toggleClass("on");
